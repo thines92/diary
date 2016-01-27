25f9a3a59b8e32f487ab2406102c308e409fcdf1
Tyler Hines <tylerhines1992@gmail.com>
2016-01-27 13:54:15 -0500

    Refactor code

--- assets/php/index.php
+++ assets/php/index.php
@@ -1,21 +1,27 @@
 <?php include("login.php"); ?>
 
-<form method="post">
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Secret Diary</title>
+	</head>
+	<body>
 	
-	<input type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']); ?>"/>
+		<form method="post">
+			
+			<input type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']); ?>"/>
+			<input type="password" name="password" id="password" />
+			<input type="submit" name="submit" value="Sign up" />
 
-	<input type="password" name="password" id="password" />
+		</form>
 
-	<input type="submit" name="submit" value="Sign up" />
+		<form method="post">
+			
+			<input type="email" name="loginEmail" id="loginEmail" value="<?php echo addslashes($_POST['loginEmail']); ?>"/>
+			<input type="password" name="loginPassword" id="loginPassword" />
+			<input type="submit" name="submit" value="Log in" />
+			
+		</form>
 
-</form>
-
-<form method="post">
-	
-	<input type="email" name="loginEmail" id="loginEmail" value="<?php echo addslashes($_POST['loginEmail']); ?>"/>
-
-	<input type="password" name="loginPassword" id="loginPassword" />
-
-	<input type="submit" name="submit" value="Log in" />
-
-</form>
+	</body>
+</html>

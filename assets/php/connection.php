48da08efb8a0a89767bf02d83a4e0928c1033498
Tyler Hines <tylerhines1992@gmail.com>
2016-01-27 13:25:54 -0500

    Refactor code

---
+++ assets/php/connection.php
@@ -0,0 +1,5 @@
+<?php
+
+	$link = mysqli_connect("localhost", "thines92", "aidran001", "diary"); // Connects to database
+
+?>
---
+++ assets/php/login.php
@@ -0,0 +1,82 @@
+<?php
+
+	session_start(); // Allows for session variables and cookies
+
+	include("connection.php");
+
+	if ($_POST['submit'] == "Sign up") {
+
+		/* ============= Email and Password Validation ===============*/
+
+		if (!$_POST['email']) $error.="</br>Please enter your email";
+			else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) $error.="</br>Please enter a valid email address";
+
+		if (!$_POST['password']) $error.="</br>Please enter your password";
+			else {
+				if (strlen($_POST['password']) < 8) $error.="</br>Please enter a password with at least 8 characters";
+				if (!preg_match('`[A-Z]`', $_POST['password'])) $error.="</br>Please include at least one capital letter in your password";
+			}
+
+		/* ============= Sign Up Code ============== */
+
+		if ($error) echo "There were error(s) in your signup details:".$error;
+		else {
+
+			if (mysqli_connect_error()) die("Could not connect to database"); // check connection to database
+
+			$query = "SELECT `email` FROM `users` WHERE `email`='".mysqli_real_escape_string/*USE THIS TO AVOID SQL INJECTION*/($link, $_POST['email'])."'"; // checks database for email that was submitted
+
+			$result = mysqli_query($link, $query);
+
+			$results = mysqli_num_rows($result); // Checks how many rows are returned.
+
+			if ($results) echo "That email address is registered. Do you want to log in?"; // if results is truthy, then an email had been found
+				else { // otherwise results is falsy and no email was found
+
+					$query = "INSERT INTO `users` (`email`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['email'])."', '".md5(md5($_POST['email']).$_POST['password'])."')";
+
+					mysqli_query($link, $query); // Runs the query and inserts user into database
+
+					echo "You've been signed up!";
+
+					$_SESSION['id'] = mysqli_insert_id($link); // Makes a session variable using the new user's ID
+
+					print_r($_SESSION); // Shows session variable
+
+					// Redirect to logged in page
+
+				}
+
+		}
+
+	}
+
+	/* ============ Log In Code ===========*/
+
+	if ($_POST['submit'] == "Log in") {
+
+			if (mysqli_connect_error()) die("Could not connect to database");
+
+			$query = "SELECT * FROM `users` WHERE `email`='".mysqli_real_escape_string($link, $_POST['loginEmail'])."' AND password='".md5(md5($_POST['loginEmail']).$_POST['loginPassword'])."' LIMIT 1";
+
+			$result = mysqli_query($link, $query);
+
+			$row = mysqli_fetch_array($result);
+
+			if ($row) {
+
+				$_SESSION['id'] = $row['id'];
+
+				print_r($_SESSION);
+
+				// Redirect to logged in page
+
+			} else {
+
+				echo "Could not find a user with that email and password. Please try again.";
+
+			}
+
+	}
+
+?>
